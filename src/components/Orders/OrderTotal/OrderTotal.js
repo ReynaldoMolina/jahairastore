3fc6949166f996c4882067ca3cce13c245e0a7a8
Reynaldo Molina <molinareynaldo952@gmail.com>
2025-03-04 16:54:57 -0600

    Added total to receipts

--- src/components/Orders/OrderTotal/OrderTotal.js
+++ src/components/Orders/OrderTotal/OrderTotal.js
@@ -8,10 +8,10 @@ function OrderTotal({ ordersGeneralTotal }) {
       <div className="flx info">
         <span className="name"></span>
         <div className="flx">
-            <span className="total-sell">{(ordersGeneralTotal.totalSell).toFixed(2)}</span>
-            <span className="total-abono">{(ordersGeneralTotal.abonos).toFixed(2)}</span>
-            <span className="total-saldo">{(ordersGeneralTotal.saldo).toFixed(2)}</span>
-            <span className="total-profit">{(ordersGeneralTotal.profit).toFixed(2)}</span>
+          <span className="total-sell">{(ordersGeneralTotal.totalSell).toFixed(2)}</span>
+          <span className="total-abono">{(ordersGeneralTotal.abonos).toFixed(2)}</span>
+          <span className="total-saldo">{(ordersGeneralTotal.saldo).toFixed(2)}</span>
+          <span className="total-profit">{(ordersGeneralTotal.profit).toFixed(2)}</span>
         </div>
       </div>
     </div>
--- src/components/Receipts/index.js
+++ src/components/Receipts/index.js
@@ -10,6 +10,7 @@ import { EmptyList } from "../EmptyList";
 import { useFilterData } from "../Hooks/useFilterData";
 import "../styles/Registers.css";
 import "./Receipts.css";
+import { ReceiptTotal } from "./ReceiptTotal/ReceiptTotal";
 
 function Receipts() {
   console.log('Render Receipts');
@@ -20,6 +21,17 @@ function Receipts() {
   
   const { data, isLoading } = useGetData(menuOption.url);
   const filteredData = useFilterData(data, menuOption.name);
+
+  let abono;
+
+  if (filteredData) {
+    abono = filteredData.reduce((sum, item) => sum + item.abono, 0);
+  }
+
+  const receiptsGeneralTotal = {
+    totalCount: filteredData.length,
+    abono
+  }
   
   return (
     <>
@@ -47,13 +59,15 @@ function Receipts() {
                       <div className="flx">
                         <span className="date">{register.saleDate}</span>
                         <span className="total">{register.orderId}</span>
-                        <span className="abono">{(register.abono).toFixed(2)}</span>
+                        <span className="abono abono-receipt">{(register.abono).toFixed(2)}</span>
                     </div>
                   </div>
                 </div>
               ))}
             </div>
           )}
+          <div className="total-separator"></div>
+          <ReceiptTotal receiptsGeneralTotal={receiptsGeneralTotal}/>
         </>
       )}
       {openModal && (
