e568bb33c4ee89196821fccc8c24f2c4236a55fa
Reynaldo Molina <molinareynaldo952@gmail.com>
2025-05-09 22:20:55 -0600

    baseUrl to api demo

--- src/app/lib/menuOptions.js
+++ src/app/lib/menuOptions.js
@@ -1,5 +1,5 @@
-// export const baseUrl = 'https://jahairastore-api.vercel.app/api/v1';
-export const baseUrl = 'http://localhost:3001/api/v1';
+export const baseUrl = 'https://jahairastore-api-demo.vercel.app/api/v1';
+// export const baseUrl = 'http://localhost:3001/api/v1';
 
 // para testear desde el telefono
 // export const baseUrl = 'http://192.168.1.7:3001/api/v1';
