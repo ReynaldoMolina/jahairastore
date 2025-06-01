5e042c647abeb545960c24b558308155a0ca8f37
Reynaldo Molina <molinareynaldo952@gmail.com>
2025-06-01 12:54:08 -0600

    Middleware to allow favicon

--- src/middleware.js
+++ src/middleware.js
@@ -4,6 +4,7 @@ import { authConfig } from './auth.config';
 export default NextAuth(authConfig).auth;
  
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|robots.txt|.*\\.png$).*)'],
+  matcher: [
+    '/((?!api|_next|favicon.ico|robots.txt|.*\\.(?:png|jpg|jpeg|svg|ico|webp)$).*)',
+  ],
 };
