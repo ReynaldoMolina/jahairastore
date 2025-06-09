64ac8d7f793024cb54d8c51e06edeb51606802cf
Reynaldo Molina <molinareynaldo952@gmail.com>
2025-06-08 23:51:56 -0600

    Changed home text style

--- src/app/ui/login/LogoutForm.jsx
+++ src/app/ui/login/LogoutForm.jsx
@@ -10,11 +10,11 @@ export default function LogoutForm() {
 
   return (
     <form
-      className="flex flex-col mt-5 justify-center items-center gap-4"
+      className="flex flex-col mt-5 justify-center items-center gap-7"
       action={formAction}
     >
       <h2
-        className="text-center text-sm font-bold bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-pink-500">¡Hola, Jahaira! ¡Vos podés!</h2>
+        className="text-center text-sm font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-sky-500">¡Hola, Jahaira! ¡Vos podés!</h2>
       <button
         className="flex p-2 rounded-xl bg-red-200 dark:bg-red-700 gap-2 items-center justify-center cursor-pointer w-38"
       >
