54d403e3b6012359b30433dc57ede017496ff57f
Reynaldo Molina <molinareynaldo952@gmail.com>
2025-07-10 10:17:23 -0600

    Added demo mode

--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,17 @@
 import NextAuth from 'next-auth';
 import { authConfig } from './auth.config';
- 
-export default NextAuth(authConfig).auth;
- 
+import { NextResponse } from 'next/server';
+
+const isDemo = process.env.NEXT_PUBLIC_APP_MODE === 'demo';
+
+export default function middleware(req) {
+  if (isDemo) {
+    return NextResponse.next();
+  }
+
+  return NextAuth(authConfig).auth(req);
+}
+
 export const config = {
   matcher: [
     '/((?!api|_next|favicon.ico|robots.txt|.*\\.(?:png|jpg|jpeg|svg|ico|webp)$).*)',
