fac5b73132d955cdea94de3dba78d9778289879e
Reynaldo Molina <molinareynaldo952@gmail.com>
2025-10-14 23:03:15 -0600

    Update logout button style

--- src/components/login/logout-form.tsx
+++ src/components/login/logout-form.tsx
@@ -4,6 +4,7 @@ import { useActionState } from 'react';
 import { LogOut } from 'lucide-react';
 import { Spinner } from '../ui/spinner';
 import { handleLogout } from '@/server-actions/actions';
+import { Button } from '../ui/button';
 
 export default function LogoutForm() {
   const [state, formAction, isPending] = useActionState(
@@ -13,23 +14,19 @@ export default function LogoutForm() {
 
   return (
     <form action={formAction} className="w-full h-full max-w-3xl">
-      <button
-        className={`flex w-full justify-center items-center rounded-lg gap-2 p-3 text-xs text-center bg-red-200 shadow dark:bg-red-800/70 ${
-          isPending
-            ? 'cursor-not-allowed'
-            : 'cursor-pointer hover:bg-red-300 dark:hover:bg-red-700/70'
-        }`}
-        disabled={isPending}
-      >
+      <Button variant="destructive" disabled={isPending} className="w-full">
         {isPending ? (
-          <Spinner />
+          <>
+            <Spinner />
+            Cerrando sesión
+          </>
         ) : (
-          <div className="flex gap-2 justify-center md:justify-start items-center font-bold text-xs">
+          <>
             <LogOut className="size-5" />
             Cerrar sesión
-          </div>
+          </>
         )}
-      </button>
+      </Button>
     </form>
   );
 }
