b74b4b8cad0497659dad70a0a8e7ffd5fa857906
Reynaldo Molina <molinareynaldo952@gmail.com>
2025-10-21 22:37:50 -0600

    Update home page cta buttons

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -8,37 +8,37 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from './ui/accordion';
+import { Button } from './ui/button';
+import { Coins, ShoppingBag } from 'lucide-react';
 
 export function Home({ businessInfo }: { businessInfo: SettingsFormType }) {
   return (
     <main className="flex justify-center items-center w-full grow p-5">
-      <section className="flex flex-col md:flex-row items-center gap-6 md:gap-10 p-4 w-full max-w-5xl">
+      <section className="flex flex-col md:flex-row items-center gap-6 md:gap-10 p-4 w-full max-w-3xl">
         {/* Text section */}
         <div className="flex flex-col items-center md:items-start md:w-[60%] text-center md:text-left gap-5 order-2 md:order-1">
           <h1 className="w-full text-3xl md:text-4xl font-extrabold bg-gradient-to-r from-purple-500 to-sky-500 bg-clip-text text-transparent">
             {businessInfo.nombreEmpresa}
           </h1>
-          <p className="w-full text-lg md:text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-fuchsia-500 to-sky-400">
-            {businessInfo.eslogan}
-          </p>
-          <p className="w-full text-sm md:text-base font-medium text-emerald-500">
+          <p className="w-full text-sm font-semibold">{businessInfo.eslogan}</p>
+          <p className="w-full text-sm text-muted-foreground">
             {isDemo ? 'DEMO' : businessInfo.mensaje || '¡Bienvenido!'}
           </p>
 
           {/* CTA Button */}
-          <div className="inline-flex gap-3">
-            <Link
-              href="/ventas/create"
-              className="mt-5 px-6 py-3 w-fit bg-gradient-to-r from-purple-600 to-sky-500 text-white font-semibold rounded-full shadow hover:scale-105 transition"
-            >
-              Crear venta
-            </Link>
-            <Link
-              href="/pedidos/create"
-              className="mt-5 px-6 py-3 w-fit bg-gradient-to-r from-purple-600 to-sky-500 text-white font-semibold rounded-full shadow hover:scale-105 transition"
-            >
-              Crear pedido
-            </Link>
+          <div className="inline-flex gap-3 flex-col sm:flex-row">
+            <Button asChild type="button">
+              <Link href="/ventas/create">
+                <Coins />
+                Crear venta
+              </Link>
+            </Button>
+            <Button asChild type="button" variant="secondary">
+              <Link href="/pedidos/create">
+                <ShoppingBag />
+                Crear pedido
+              </Link>
+            </Button>
           </div>
           {!isDemo && (
             <Accordion type="single" collapsible className="w-full">
