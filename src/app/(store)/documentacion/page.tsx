b8a5eebd4c4508bcc0959331677139e27f096330
Reynaldo Molina <molinareynaldo952@gmail.com>
2025-12-15 22:36:44 -0600

    Dont show documentacion on demo mode

--- src/app/(store)/documentacion/page.tsx
+++ src/app/(store)/documentacion/page.tsx
@@ -2,6 +2,8 @@ import { checkAuthorization } from '@/authorization/check-authorization';
 import { Documentacion } from '@/components/documentacion/documentacion';
 import { PageWrapper } from '@/components/page-wrapper';
 import { SiteHeader } from '@/components/site-header';
+import { isDemo } from '@/middleware';
+import { notFound } from 'next/navigation';
 
 export const metadata = {
   title: 'Documentación',
@@ -9,6 +11,8 @@ export const metadata = {
 
 export default async function Page() {
   await checkAuthorization();
+
+  if (isDemo) return notFound();
 
   return (
     <>
