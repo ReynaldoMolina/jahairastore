d3c12b4c4f572238c302a04821dd28755281d125
Reynaldo Molina <molinareynaldo952@gmail.com>
2026-01-10 21:09:51 -0600

    Remove balance ping animation

--- src/components/lists/ping-div.tsx
+++ src/components/lists/ping-div.tsx
@@ -6,8 +6,8 @@ interface PingDivProps {
 
 export function PingDiv({ className }: PingDivProps) {
   return (
-    <div className={cn(className, 'size-2.5 bg-destructive rounded-full')}>
-      <div className="size-2.5 bg-destructive rounded-full animate-ping"></div>
-    </div>
+    <div
+      className={cn(className, 'size-2.5 bg-destructive rounded-full')}
+    ></div>
   );
 }
