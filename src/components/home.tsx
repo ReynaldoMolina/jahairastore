c2070900923180a47f753e10558c5e894c9cef6f
Reynaldo Molina <molinareynaldo952@gmail.com>
2026-01-10 20:57:13 -0600

    Remove Home page

--- src/components/home.tsx
+++
@@ -1,9 +0,0 @@
-import { BusinessInfoType } from '@/types/types';
-
-export function Home({ businessInfo }: { businessInfo: BusinessInfoType }) {
-  return (
-    <span className="text-sm font-extrabold bg-gradient-to-r from-purple-500 to-sky-500 bg-clip-text text-transparent my-2">
-      {businessInfo.eslogan}
-    </span>
-  );
-}
