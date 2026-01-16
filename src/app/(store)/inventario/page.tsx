0d37dcce287f5390d89afa624ff29cdb08dab7f2
Reynaldo Molina <molinareynaldo952@gmail.com>
2026-01-16 11:13:59 -0600

    Add suspense boundary to inventory

--- src/app/(store)/inventario/page.tsx
+++ src/app/(store)/inventario/page.tsx
@@ -1,13 +1,11 @@
 import { checkAuthorization } from '@/authorization/check-authorization';
 import { HeaderFilter } from '@/components/filters/header-filter';
-import { SearchInput } from '@/components/filters/search-input';
-import { Products } from '@/components/lists/products';
 import { PageWrapper } from '@/components/page-wrapper';
 import { SiteHeader } from '@/components/header/site-header';
-import { getProducts } from '@/fetch-data/product';
 import { PageProps } from '@/types/types';
-import { StockLocationFilter } from '@/components/filters/stock-location';
-import { ExportInventory } from '@/components/export-to-excel';
+import { Inventory } from '@/components/lists/inventory/inventory';
+import { Suspense } from 'react';
+import { Spinner } from '@/components/ui/spinner';
 
 export async function generateMetadata({ searchParams }: PageProps) {
   const ubicacion = (await searchParams).ubicacion || '';
@@ -22,27 +20,26 @@ export async function generateMetadata({ searchParams }: PageProps) {
 export default async function Page({ searchParams }: PageProps) {
   await checkAuthorization();
 
-  const { data, query, totalPages } = await getProducts(await searchParams);
   const ubicacion = (await searchParams).ubicacion || '';
-  const label =
+  const ubicacionLabel =
     ubicacion === '' ? 'Total' : ubicacion === '1' ? 'León' : 'Acoyapa';
 
   return (
     <>
       <SiteHeader
-        title={`Inventario - ${label}`}
+        title={`Inventario - ${ubicacionLabel}`}
         showHeaderActions
         hideBackButton
       >
         <HeaderFilter listName="inventario" />
       </SiteHeader>
       <PageWrapper>
-        <div className="flex gap-1">
-          <SearchInput showScanButton />
-          <StockLocationFilter />
-          <ExportInventory data={data} label={label} />
-        </div>
-        <Products data={data} query={query} totalPages={totalPages} />
+        <Suspense fallback={<Spinner className="m-auto" />}>
+          <Inventory
+            searchParams={await searchParams}
+            ubicacionLabel={ubicacionLabel}
+          />
+        </Suspense>
       </PageWrapper>
     </>
   );
---
+++ src/components/lists/inventory/inventory.tsx
@@ -0,0 +1,29 @@
+import { ExportInventory } from '@/components/export-to-excel';
+import { SearchInput } from '@/components/filters/search-input';
+import { StockLocationFilter } from '@/components/filters/stock-location';
+import { Products } from '../products';
+import { getProducts } from '@/fetch-data/product';
+import { SearchParamsProps } from '@/types/types';
+
+interface InventoryProps {
+  searchParams: SearchParamsProps;
+  ubicacionLabel: string;
+}
+
+export async function Inventory({
+  searchParams,
+  ubicacionLabel,
+}: InventoryProps) {
+  const { data, query, totalPages } = await getProducts(searchParams);
+
+  return (
+    <>
+      <div className="flex gap-1">
+        <SearchInput showScanButton />
+        <StockLocationFilter />
+        <ExportInventory data={data} label={ubicacionLabel} />
+      </div>
+      <Products data={data} query={query} totalPages={totalPages} />
+    </>
+  );
+}
