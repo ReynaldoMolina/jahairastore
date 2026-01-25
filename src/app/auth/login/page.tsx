adf9375ff59e8468cae81d95f193074b8e172da7
Reynaldo Molina <molinareynaldo952@gmail.com>
2026-01-25 16:24:13 -0600

    Add store name to login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from '@/components/form/login/login-form';
 import { Suspense } from 'react';
 
 export const metadata = {
-  title: 'Iniciar sesión',
+  title: 'Iniciar sesión - Jahaira Store',
 };
 
 export default function Page() {
