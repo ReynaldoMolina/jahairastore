cbdb38aba5bda823f99ffb9a1cb8df4449bac027
Reynaldo Molina <molinareynaldo952@gmail.com>
2026-01-27 13:30:44 -0600

    Update dashboard title

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -3,25 +3,19 @@ import { PageWrapper } from '@/components/page-wrapper';
 import { Dashboard } from '@/components/dashboard/dashboard';
 import { SiteHeader } from '@/components/header/site-header';
 import { PageProps } from '@/types/types';
-import { getBusinessInfo, getBusinessName } from '@/fetch-data/settings';
 import { Suspense } from 'react';
 import Loading from '@/components/loading';
 
-export async function generateMetadata() {
-  const businessName = await getBusinessName();
-
-  return {
-    title: businessName.nombreEmpresa || 'Tienda',
-  };
-}
+export const metadata = {
+  title: 'Dashboard',
+};
 
 export default async function Page({ searchParams }: PageProps) {
   await checkAuthorization();
-  const businessInfo = await getBusinessInfo();
 
   return (
     <>
-      <SiteHeader title={businessInfo.nombreEmpresa} hideBackButton />
+      <SiteHeader title="Dashboard" hideBackButton />
       <PageWrapper>
         <Suspense fallback={<Loading />}>
           <Dashboard searchParams={await searchParams} />
